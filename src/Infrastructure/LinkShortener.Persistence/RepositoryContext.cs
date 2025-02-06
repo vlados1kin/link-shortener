84d9e3e720e4fa1b4fa9a140a56d8f648e2bd184
vlados1kin <lashkin2004@gmail.com>
2025-02-06 19:10:06 +0300

    Create repository context

---
+++ src/Infrastructure/LinkShortener.Persistence/RepositoryContext.cs
@@ -0,0 +1,21 @@
+﻿using LinkShortener.Domain.Entities;
+using LinkShortener.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace LinkShortener.Persistence;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions<RepositoryContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Url> Urls { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UrlConfiguration());
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
