482a550883b43db119ccb6b6d12cb6da7c15ffaf
vlados1kin <lashkin2004@gmail.com>
2025-02-06 19:11:25 +0300

    Add extension methods and configure the repository context and the url repository

---
+++ src/Infrastructure/LinkShortener.Persistence/Extensions/ServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using LinkShortener.Domain.Interfaces;
+using LinkShortener.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LinkShortener.Persistence.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureRepositoryContext(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<RepositoryContext>(options =>
+        {
+            options.UseMySQL(configuration.GetConnectionString("Database")!, builder => builder.MigrationsAssembly(nameof(Persistence)));
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection ConfigureRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUrlRepository, UrlRepository>();
+
+        return services;
+    }
+}
--- src/Presentation/LinkShortener.API/Program.cs
+++ src/Presentation/LinkShortener.API/Program.cs
@@ -1,7 +1,12 @@
+using LinkShortener.Persistence.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.ConfigureRepositoryContext(builder.Configuration);
+builder.Services.ConfigureRepositories();
 
 var app = builder.Build();
 
