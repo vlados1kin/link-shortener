29d9b0a317eabd9136ad4b8ac5323923e6127da9
vlados1kin <lashkin2004@gmail.com>
2025-02-06 19:09:11 +0300

    Create an entity

---
+++ src/Core/LinkShortener.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace LinkShortener.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
---
+++ src/Core/LinkShortener.Domain/Entities/Url.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkShortener.Domain.Entities;
+
+public class Url : BaseEntity
+{
+    public string LongUrl { get; set; } = null!;
+    public string ShortUrl { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public int ClickCount { get; set; } = 0;
+}
