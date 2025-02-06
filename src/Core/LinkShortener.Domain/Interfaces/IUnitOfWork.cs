3afe8a6c770fdd82f36a3e87c78e934ac97cc823
vlados1kin <lashkin2004@gmail.com>
2025-02-06 19:49:12 +0300

    Implement the unit of work pattern

---
+++ src/Core/LinkShortener.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace LinkShortener.Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    IUrlRepository Url { get; }
+    Task SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ src/Infrastructure/LinkShortener.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using LinkShortener.Domain.Interfaces;
+
+namespace LinkShortener.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly RepositoryContext _repositoryContext;
+    private readonly Lazy<IUrlRepository> _urlRepository;
+
+    public UnitOfWork(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+        _urlRepository = new Lazy<IUrlRepository>(() => new UrlRepository(repositoryContext));
+    }
+
+    public IUrlRepository Url => _urlRepository.Value;
+    
+    public async Task SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        await _repositoryContext.SaveChangesAsync(cancellationToken);
+    }
+}
