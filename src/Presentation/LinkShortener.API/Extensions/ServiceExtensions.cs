85cf88d4924eab9e33142663d1c5d32254da6e9a
vlados1kin <lashkin2004@gmail.com>
2025-02-07 11:26:05 +0300

    Configure cors and migrations

---
+++ src/Presentation/LinkShortener.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkShortener.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("LinkShortenerPolicy", policyBuilder =>
+            {
+                policyBuilder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+        });
+
+        return services;
+    }
+}
--- src/Presentation/LinkShortener.API/Program.cs
+++ src/Presentation/LinkShortener.API/Program.cs
@@ -1,3 +1,5 @@
+using LinkShortener.API.Extensions;
+using LinkShortener.Persistence;
 using LinkShortener.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,8 +9,17 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.ConfigureRepositoryContext(builder.Configuration);
 builder.Services.ConfigureRepositories();
+builder.Services.ConfigureCors();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var repositoryContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>();
+    await repositoryContext.Database.MigrateAsync();
+}
+
+app.UseCors("LinkShortenerPolicy");
 
 if (app.Environment.IsDevelopment())
 {
