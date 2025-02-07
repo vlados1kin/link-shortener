a610c468f0e794a0e716200543ca28f62a064bc1
vlados1kin <lashkin2004@gmail.com>
2025-02-07 11:53:58 +0300

    Add custom exceptions

---
+++ src/Core/LinkShortener.Application/Exceptions/ExceptionMessages.cs
@@ -0,0 +1,6 @@
+﻿namespace LinkShortener.Application.Exceptions;
+
+public static class ExceptionMessages
+{
+    public const string UrlNotFound = "Url with: {0} does not exist in the database";
+}
